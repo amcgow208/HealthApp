92342440c7cb95e8afdb8c15854feddc5c618891
Andrew McGowan <amcgow208@caledonian.ac.uk>
2022-03-28 12:40:32 +0100

    Controller

--- src/app/HealthApp.java
+++ src/app/HealthApp.java
@@ -1,4 +1,15 @@
 package app;
 
+import controllers.Controller;
+
 public class HealthApp {
+
+    private static void run() {
+        final Controller controller = new Controller();
+        controller.run();
+    }
+    public static void main(final String[] args) {
+        HealthApp.run();
+    }
+
 }
--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -1,4 +1,44 @@
 package controllers;
 
+import java.util.List;
+
+import helpers.InputHelper;
+import model.User;
+import model.HealthData;
+import model.BMI;
+import repositories.Repository;
+
 public class Controller {
+
+    private final Repository repository;
+    public Controller() {repository = new Repository();}
+
+
+    public void run(){
+        boolean finished = false;
+        InputHelper inputHelper = new InputHelper();
+        do {
+            System.out.print("\nA. Display User Information");
+            System.out.print("\tB. Update User Information");
+            System.out.print("\tC. View BMI");
+            System.out.print("\tQ. Quit\n");
+            char choice = inputHelper.readCharacter("Enter choice", "ABCQ");
+            switch (choice) {
+                case 'A':
+                    displayUser();
+                    break;
+                case 'Q':
+                    finished = true;
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + choice);
+            }
+        } while (!finished);
+    }
+    public void displayUser() {
+        System.out.println("User Info");
+        System.out.println("===========");
+        List<User> userList = repository.getUserInformation();
+        System.out.println(userList);
+    }
 }
